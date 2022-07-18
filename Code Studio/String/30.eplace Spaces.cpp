700bdce4c8800af66089143c8b1f888288e32576
Amit Kumar <amit.engg9897@gmail.com>
2022-07-19 00:38:50 +0530

    repalce question

---
+++ Code Studio/String/30.eplace Spaces.cpp
@@ -0,0 +1,16 @@
+string replaceSpaces(string &str){
+   for(int i=0; i<str.length(); i++){
+
+       if(str[i]==' '){
+
+           str.erase(i, 1); //method 2
+
+           str.insert(i, "@40");
+
+       }
+
+   }
+
+   return str;
+
+}

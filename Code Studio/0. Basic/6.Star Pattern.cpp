81d9ef28d81f746cb501344d6a3980bdc50d56e5
Amit Kumar <amit.engg9897@gmail.com>
2022-07-19 22:06:32 +0530

    question

---
+++ Code Studio/0. Basic/6.Star Pattern.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+       /*  Read input as specified in the question.
+    * Print output as specified in the question.
+    */
+    int n;
+    cin>>n;
+    for(int i=1; i<=n; i++)
+    {
+        for(int j=0; j<n-i; j++)
+        {
+            cout<<" ";
+        }
+        for(int j=1; j<2*i; j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}

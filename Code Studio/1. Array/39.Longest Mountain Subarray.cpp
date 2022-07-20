d728089cea184b92ac3a390c53e831d56f446fa9
Amit Kumar <amit.engg9897@gmail.com>
2022-07-20 15:29:30 +0530

    question

---
+++ Code Studio/1. Array/39.Longest Mountain Subarray.cpp
@@ -0,0 +1,78 @@
+int longestMountain(int *arr, int n)
+
+{
+
+   if(n < 3)
+
+   {
+
+    return 0;
+
+   }
+
+   int start = -1, end = -1;
+
+   int ans = 0;
+    
+   for (int i = 0; i < n - 1; ++i)
+   {
+
+    if(arr[i+1] > arr[i])
+
+    {
+     if(end != -1) {
+
+      end = -1;
+
+      start = -1;
+
+     }
+     if(start == -1) {
+
+      start = i;
+
+     }
+
+    }
+
+    else
+
+    {
+
+     if(arr[i+1] < arr[i])
+
+     {
+
+      if(start != -1) {
+
+       end = i + 1;
+
+      }
+      if(end != -1 && start != -1) {
+
+       ans = max(ans, end - start + 1);
+
+      }
+
+     }
+
+     else
+
+     {
+
+      start = -1;
+      end = -1;
+     }
+
+    }
+
+   }
+   if(end != -1 && start != -1)
+
+   {
+
+    ans = max(ans, end - start + 1);
+
+   }
+   return ans;
+}

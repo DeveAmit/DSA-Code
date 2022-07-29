b9b701282eba89b5dbf5b9e3de18dae0ba1bd927
Amit Kumar <amit.engg9897@gmail.com>
2022-07-29 18:57:47 +0530

    qquestion

---
+++ Leetcode/2.String/1370. Increasing Decreasing String.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string sortString(string s) {
+        
+        vector<int>freq(150, 0);
+        
+        for(char ch:s)
+            freq[ch]++;
+        
+        string res = "";
+        
+        bool found = true;
+        while(found)
+        {
+            found = false;
+            for(char c='a'; c<='z'; c++)
+                if (freq[c])
+                {
+                    res.push_back(c);
+                    freq[c]--;
+                    found = true;
+                }
+
+            for(char c='z'; c>='a'; c--)
+                if(freq[c])
+                {
+                    res.push_back(c);
+                    freq[c]--;
+                    found = true;
+                }
+        }
+        
+        return res;
+    }
+};

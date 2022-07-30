64f3f6af317d9a8cd1002046137e216e483ea67c
Amit Kumar <amit.engg9897@gmail.com>
2022-07-30 20:21:43 +0530

    new

---
+++ Code Studio/2. String/18.Left And Right Rotation.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+string leftRotate(string str, int d) {
+    d=d%str.length();
+    return str.substr(d)+str.substr(0,d);
+}
+
+string rightRotate(string str, int d) {
+    d=d%str.length();
+    return str.substr(str.length()-d)+str.substr(0,str.length()-d);
+}
